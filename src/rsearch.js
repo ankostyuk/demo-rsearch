df5f737a59b13ce05ebfa2060a334326120609d4
ankostyuk <andrew.kostyuk@gmail.com>
2014-06-17 14:32:43 +0400

    Шаблон основного модуля

---
+++ src/rsearch.js
@@ -0,0 +1,11 @@
+/**
+ * @module rsearch
+ * @desc RequireJS/Angular module
+ * @author ankostyuk
+ */
+define(function(require) {'use strict';
+
+    var angular = require('angular');
+
+    return angular.module('np.rsearch', []);
+});
